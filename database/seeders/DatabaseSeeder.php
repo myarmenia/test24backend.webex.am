9daf2ea7a5a13ae52994f2556f7f4d8e80a96b0b
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-07-15 17:22:30 +0400

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-       
-        $this->call(CategoriesTableSeeder::class);
+
+        $this->call([CategoriesTableSeeder::class]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
